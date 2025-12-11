788863ef108ef4ec52ed726280eaeaadb920c846
ahmed-abdelrady-SE <22abdelrady22@gmail.com>
2025-12-11 23:09:55 +0200

    first commit

---
+++ Linked Lists/LinkedList.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class node{      // the main class for the nodes in the linked list 
+    int data;  
+    node* next;
+};
+
+class LinkedList{
+
+};
+
+int main(){
+
+}
