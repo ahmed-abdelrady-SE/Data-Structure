f79cee21e7c6db03fee80e65bf3c0e1c85864f4d
ahmed-abdelrady-SE <22abdelrady22@gmail.com>
2025-12-25 19:23:30 +0200

    stack using array

--- Stack/StackUsingArray.cpp
+++ Stack/StackUsingArray.cpp
@@ -1,8 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX_SIZE = 100;  // a global variable that cannot be changed to refer to the max size
 
+class Stack_array{
+    public :
+
+    int top;
+    int stack[100];
+
+    Stack_array(){
+        top = -1;
+    }
+
+    bool is_empty(){
+        return top == -1;
+    }
+
+    void push(int data){
+        if(top == MAX_SIZE - 1){
+            cout << "the stack is full\n";
+            return;
+        }
+        stack[++top] = data;
+    }
+
+    void pop(){
+        if( is_empty() ){
+            cout << "the stack is empty there is nothing to pop\n";
+            return;
+        }
+        top--;
+    }
+
+    int peek(){
+        if( is_empty() ){
+            cout << "the stack is empty\n";
+            return -1;
+        }
+
+        return stack[top];
+
+    }
+
+    int size(){
+        return top+1;
+    }
+
+    void print(){
+        for(int i=top; i>= 0; i--){   // we have to traverse backward because the stack follows the LIFO principles 
+            cout << stack[i] << " ";
+        }
+        cout << "\n";
+    }
+
+};
 
 int main(){
-    
+    Stack_array stack;
 }
